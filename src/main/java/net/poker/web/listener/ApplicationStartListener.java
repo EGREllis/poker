d4882103cb8f305864b90c83eed208029647cf5c
egrellis <edwardellis.egre@gmail.com>
2020-06-16 15:44:34 +0100

    Removed unneccessary logging

--- src/main/java/net/poker/web/listener/ApplicationStartListener.java
+++ src/main/java/net/poker/web/listener/ApplicationStartListener.java
@@ -4,10 +4,8 @@ import net.poker.data.RegisterUser;
 import net.poker.data.SignInUser;
 import net.poker.data.simple.UserMap;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ApplicationStartListener implements ServletContextListener {
@@ -20,9 +18,6 @@ public class ApplicationStartListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         logger.info("Context initialized (info): "+servletContextEvent.getServletContext().getServletContextName());
-        logger.log(Level.ALL, "Context initialized (all): "+servletContextEvent.getServletContext().getServletContextName());
-        System.err.println("Context initialized (stderr):" +servletContextEvent.getServletContext().getServletContextName());
-        System.out.println("Context initialized (stdout):" +servletContextEvent.getServletContext().getServletContextName());
 
         UserMap userMap = new UserMap();
         servletContextEvent.getServletContext().setAttribute(RegisterUser.KEY, userMap);
@@ -32,7 +27,5 @@ public class ApplicationStartListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
         logger.info("Context destroyed: "+servletContextEvent.getServletContext().getServletContextName());
-        System.err.println("Context destroyed (stderr):" +servletContextEvent.getServletContext().getServletContextName());
-        System.out.println("Context initialized (stdout):" +servletContextEvent.getServletContext().getServletContextName());
     }
 }
